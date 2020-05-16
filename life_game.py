5b74d0985aad887c82899075491522bebaaffc7e
Ricky <64746753+Lepechou@users.noreply.github.com>
2020-05-16 18:23:15 -0400

    Update life_game.py

--- life_game.py
+++ life_game.py
@@ -11,13 +11,13 @@ import random
 
 
 
-n = 10
+n = 10  # number of rows and columns we want
 a = np.array([[0]* n for i in range (n)])
 
 
 def creation(board):
-    for row in board:
-        for column in row:
+    for row in range(n):
+        for column in range(n):
             board[row][column] = random.randrange(2)
     return board
 
