279d7bee36c6a1f390113a0b2f196b02b425809b
Ricky <64746753+Lepechou@users.noreply.github.com>
2020-05-17 15:16:36 -0400

    Update life_game.py

--- life_game.py
+++ life_game.py
@@ -26,12 +26,16 @@ def creation(board):
 Initial_Board = creation(Board_Base)
 
 
-def cell_state(board):
-    while True:
-        new_board = []
-        for row in range(n):
-            for column in range(n):
-                neighbours = 0
+def next_state(board):
+    new_board = np.array([[0] * columns for i in range(rows)])
+    for row in range(rows -1):
+        if (row - 1) < 0 or (row + 1) > rows:
+            continue
+        for column in range(columns - 1):
+            neighbours = 0
+            if (column - 1) < 0 or (column + 1) > columns:
+                continue
+            else:
                 neighbours += board[row - 1][column + 1]
                 neighbours += board[row][column + 1]
                 neighbours += board[row + 1][column + 1]
@@ -40,16 +44,16 @@ def cell_state(board):
                 neighbours += board[row - 1][column - 1]
                 neighbours += board[row][column - 1]
                 neighbours += board[row + 1][column - 1]
-                if neighbours == 2 or 3:                      # Normal :If 2 or 3 neighbours changes state to alive: 1
+                if neighbours == 2 :  # Normal :If 2 or 3 neighbours changes state to alive: 1
                     new_board[row][column] = 1
-                elif neighbours == 0 or 1:                    # Underpopulation: If 0 or 1 neighbours changes state to dead: 0
+                elif neighbours == 0 :  # Underpopulation: If 0 or 1 neighbours changes state to dead: 0
                     new_board[row][column] = 0
-                else:                                         # Overpopulation : change state to dead: 0
+                elif neighbours > 4:  # Overpopulation : change state to dead: 0
                     new_board[row][column] = 0
-        yield new_board
+    return new_board
 
 
-cell_state(Initial_Board)
+next_state(Initial_Board)
 
 
 
